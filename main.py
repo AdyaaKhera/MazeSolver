b1a0e733c4544bfedc03d751bee4093850323c7c
adyaakhera <99728646+AdyaaKhera@users.noreply.github.com>
2025-07-03 21:23:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ window = pygame.display.set_mode((window_width, window_width))
 pygame.display.set_caption("Maze Solver")
 
 #defining colors
-white = (255, 255, 255)  # wall
-black = (0, 0, 0)        # background
+white = (255, 255, 255) 
+black = (0, 0, 0)      
+green = (0, 255, 0)
+red = (255, 0, 0)  
 gridlines = (50, 50, 50)
 
 rows = 30  #setting grid size 30x30
@@ -37,6 +39,18 @@ class Node:
 
     def reset(self): #resets the node to background
         self.color = black
+    
+    def make_start(self): #creates a starting node
+        self.color = green
+
+    def make_end(self): #creates an ending node
+        self.color = red
+
+    def is_start(self): #checks if  a node is a starting node
+        return self.color == green
+    
+    def is_end(self): #checks if a node is an ending node
+        return self.color == red
 
 
 def make_grid(rows, width): #function to make the 30x30 grid
@@ -69,15 +83,34 @@ def draw(win, grid, rows, width): #function to draw the entire screen
     pygame.display.update() #window gets updated from scratch everytime 
 
 
-def main(win, width): #main loop
-    grid = make_grid(rows, width)
-
+def main(win, window_width): #main loop
+    grid = make_grid(rows, window_width)
+    start = None
+    end = None
+    cell_size = window_width // rows #sizing each square
     run = True
     while run:
-        draw(win, grid, rows, width) #this is where the window gets updated
+        draw(win, grid, rows, window_width) #this is where the window gets updated
         for event in pygame.event.get(): #checking if the user is trying to close the window
             if event.type == pygame.QUIT:
                 run = False
+            if pygame.mouse.get_pressed()[0]: #if left mouse button is pressed, we get the current position of the cursor in pixels
+                pos = pygame.mouse.get_pos()
+                row = pos[0] // cell_size
+                col = pos[1] // cell_size
+                #ensuring the mouse is within the limits of the grid
+                if row < rows and col < rows:
+                    node = grid[row][col] #fetching the node object at the given position
+                    node.make_wall() #setting color to white to make that a wall
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 3: #if right mouse button is pressed, the wall will get removed
+                    pos = pygame.mouse.get_pos()
+                    row = pos[0] // cell_size
+                    col = pos[1] // cell_size
+                    #ensuring the mouse is within the limits of the grid
+                    if row < rows and col < rows:
+                        node = grid[row][col] #fetching the node object at the given position
+                        node.reset()
 
     pygame.quit() #cleaning up the window
 
